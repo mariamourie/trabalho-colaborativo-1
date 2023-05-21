f25ca93d18403f3832a8d4f5548bef4ad624224b
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2023-05-21 14:05:49 -0300

    Estrutura básica dos problemas

---
+++ Problema 4/src/application/TesteRetangulo.java
@@ -0,0 +1,9 @@
+package application;
+
+public class TesteRetangulo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Problema 4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maria
+ *
+ */
+module Problema4 {
+}
