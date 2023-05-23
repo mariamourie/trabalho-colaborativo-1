a2007e2e3f31b2d0583f348245609cc526c3dd34
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2023-05-23 09:53:22 -0300

    Problem 4

--- Problema 4/src/application/TesteRetangulo.java
+++ Problema 4/src/application/TesteRetangulo.java
@@ -1,9 +1,26 @@
 package application;
 
+import entities.Retangulo;
+
 public class TesteRetangulo {
 
 	public static void main(String[] args) {
-
+		Retangulo ret1 = new Retangulo();
+		Retangulo ret2 = new Retangulo();
+		
+		ret1.setComprimento(2.0);
+		ret1.setLargura(5.0);
+		System.out.println("--RETÂNGULO 1--");
+		System.out.printf("Perímetro: %.2f%n", ret1.perimetro());
+		System.out.printf("Área: %.2f%n", ret1.area());
+		
+		System.out.println();
+		
+		ret2.setComprimento(3.0);
+		ret2.setLargura(4.0);
+		System.out.println("--RETÂNGULO 2--");
+		System.out.printf("Perímetro: %.2f%n", ret2.perimetro());
+		System.out.printf("Área: %.2f%n", ret2.area());
 	}
 
 }
--- Problema 4/src/entities/Retangulo.java
+++ Problema 4/src/entities/Retangulo.java
@@ -1,5 +1,39 @@
 package entities;
 
 public class Retangulo {
-
+	private Double comprimento;
+	private Double largura;
+	
+	public Retangulo() {
+		
+	}
+	
+	public Retangulo(double comprimento, double largura) {
+		this.comprimento = comprimento;
+		this.largura = largura;
+	}
+	
+	public  double getComprimento() {
+		return this.comprimento;
+	}
+	
+	public void setComprimento(double comprimento) {
+		this.comprimento = comprimento;
+	}
+	
+	public double getLargura() {
+		return this.largura;
+	}
+	
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
+	
+	public double perimetro() {
+		return (this.largura * 2) + (this.comprimento * 2);
+	}
+	
+	public double area() {
+		return this.largura * this.comprimento;
+	}
 }
