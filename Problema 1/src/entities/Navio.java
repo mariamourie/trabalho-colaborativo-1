58566969bc6a74294404cf2e76a4a7ee1dd80e69
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2023-05-23 10:58:45 -0300

    Navio class

--- Problema 1/src/entities/Navio.java
+++ Problema 1/src/entities/Navio.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Navio {
+	
+	private String nome;
+	private String ano;
+		
+	public Navio(String nome, String ano) {
+		this.nome = nome;
+		this.ano = ano;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getAno() {
+		return ano;
+	}
+	
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
 
+	@Override
+	public String toString() {
+		return "Navio de nome " + nome + " construído no ano de " + ano;
+	}
 }
