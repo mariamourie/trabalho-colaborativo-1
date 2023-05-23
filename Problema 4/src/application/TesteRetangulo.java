1851b506b7f048465c2c5c0e3228355cb42ca0fc
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2023-05-23 10:38:21 -0300

    TesteRetangulo modified

--- Problema 4/src/application/TesteRetangulo.java
+++ Problema 4/src/application/TesteRetangulo.java
@@ -1,26 +1,41 @@
 package application;
 
 import entities.Retangulo;
+import java.util.Scanner;
 
 public class TesteRetangulo {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		Retangulo ret1 = new Retangulo();
 		Retangulo ret2 = new Retangulo();
 		
-		ret1.setComprimento(2.0);
-		ret1.setLargura(5.0);
+		double comprimento;
+		double largura;
+		
 		System.out.println("--RETÂNGULO 1--");
+		System.out.println("Insira um valor para o comprimento");
+		comprimento = sc.nextDouble();
+		System.out.println("Insira um valor para a largura");
+		largura = sc.nextDouble();
+		ret1.setComprimento(comprimento);
+		ret1.setLargura(largura);
 		System.out.printf("Perímetro: %.2f%n", ret1.perimetro());
 		System.out.printf("Área: %.2f%n", ret1.area());
 		
 		System.out.println();
 		
-		ret2.setComprimento(3.0);
-		ret2.setLargura(4.0);
 		System.out.println("--RETÂNGULO 2--");
+		System.out.println("Insira um valor para o comprimento");
+		comprimento = sc.nextDouble();
+		System.out.println("Insira um valor para a largura");
+		largura = sc.nextDouble();
+		ret2.setComprimento(comprimento);
+		ret2.setLargura(largura);
 		System.out.printf("Perímetro: %.2f%n", ret2.perimetro());
 		System.out.printf("Área: %.2f%n", ret2.area());
+	
+		sc.close();
 	}
-
 }
